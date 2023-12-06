5dcd8d356d24e9011ee6b574b12bbf57a2b4ea2a
kirtanakrishnan <kirtana.k8@gmail.com>
2023-12-06 12:29:20 -0500

    admin and user home content

--- src/homeAdmin/homeAdmin.js
+++ src/homeAdmin/homeAdmin.js
@@ -3,18 +3,45 @@ import { React, useState } from "react";
 function HomeAdmin (
     { movies, movie, setMovie, users, user, setUser }
     ) {  
-    // const sortedMovies = [...movies].sort((a, b) => b.likes - a.likes);
+    const sortedMovies = [...movies].sort((a, b) => b.likes - a.likes);
         
-    // const top4Movies = sortedMovies.slice(0, 4);
+    const top4Movies = sortedMovies.slice(0, 4);
 
-    const sortedUsers = [...users].sort((a, b) => (b.followers ? b.followers.length : 0) - (a.followers ? a.followers.length : 0));
-    const top4Users = sortedUsers.slice(0, 4);
+    const sortedUsers = [...users];
+    
+    const top4Users = sortedUsers.slice(-4);
     
     return (
       <div className="container-fluid">
-        {/* <div className="row row-title">
+        
+       
+      <div className="row row-title">
           <div className="col-12 top-title">
-            Top Movies of 2023
+            Hi {user.firstName}!
+            <hr className="top-line" />
+            <h3 className="your-lm">Recently Joined Users</h3>
+            
+            
+          </div>
+        </div>
+
+      <div className="row row-dashboard flex-row flex-wrap d-flex">
+        {top4Users.map((user) => (
+          <div key={user._id} className="col-12 col-md-6 col-xl-3">
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="/images/purple.png" className="card-img-top" alt="..." />
+              <div className="card-body card-user">
+                <h5 className="card-title">{user.username}</h5>
+                <p className="card-text">{user.followers ? user.followers.length : 0} Followers</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="row row-title">
+          <div className="col-12 top-title">
+            <h3 className="fl">Top Performing Movies</h3>
             <hr className="top-line" />
             
             
@@ -36,33 +63,8 @@ function HomeAdmin (
               </div>
             </div>
           ))}
-        </div> */}
+        </div>
 
-        <div className="row row-title">
-          <div className="col-12 top-title">
-            Top Movie Magic Users
-            <hr className="top-line" />
-            
-            
-          </div>
-        </div>
-        
-        <div className="row row-dashboard flex-row flex-wrap d-flex">
-        
-  
-          {top4Users.map((user) => (
-            <div key={user._id} className="col-12 col-md-6 col-xl-3">
-              <div className="card" style={{ width: "18rem" }}>
-                <img src="/images/purple.png" className="card-img-top" alt="..." />
-                <div className="card-body card-user">
-                  <h5 className="card-title">{user.username}</h5>
-                  <p className="card-text">{user.followers ? user.followers.length : 0} Followers</p>
-              
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     );
   }
